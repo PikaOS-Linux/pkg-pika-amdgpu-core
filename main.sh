4ee8374f886d997c909a8abede0cf3364ca3a6af
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-02-25 10:54:57 +0300

    fix files

--- main.sh
+++ main.sh
@@ -4,9 +4,7 @@ add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
-git clone https://github.com/lassekongo83/adw-gtk3
-cp -rvf ./debian ./adw-gtk3/
-cd ./adw-gtk3
+cd ./pika-amdgpu-core
 
 # Get build deps
 apt-get build-dep ./ -y
